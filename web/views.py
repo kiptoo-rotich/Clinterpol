451ea6a5bb01ef0379bf974e8cc4c64a19d49454
kiptoo-rotich <rotichenoch27@gmail.com>
2022-02-11 02:39:32 -0500

    Integrating with M-Pesa

--- web/views.py
+++ web/views.py
@@ -2,7 +2,7 @@ import json
 from math import ceil
 
 # import pywhatkit as py
-
+import base64
 import requests
 from django.core.mail import BadHeaderError, send_mail
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -30,37 +30,58 @@ def about(request):
 
 def clients(request):
     return render(request,"main/our_clients.html")
-    
-def events(request):
+
+class MpesaC2bCredential:
+    consumer_key = 'CHrYGbGBVAqyzI35W84USB9ro8T9m22r'
+    consumer_secret = 'Zv53iyFKaJIS6AJ7'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+class MpesaAccessToken:
+    r = requests.get(MpesaC2bCredential.api_URL,
+                     auth=HTTPBasicAuth(MpesaC2bCredential.consumer_key, MpesaC2bCredential.consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token['access_token']
+class LipanaMpesaPpassword:
+    lipa_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    Business_short_code = "174379"
+    passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+    data_to_encode = Business_short_code + passkey + lipa_time
+    online_password = base64.b64encode(data_to_encode.encode())
+    decode_password = online_password.decode('utf-8')
+
+def getAccessToken(request):
+    consumer_key = 'CHrYGbGBVAqyzI35W84USB9ro8T9m22r'
+    consumer_secret = 'Zv53iyFKaJIS6AJ7'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token['access_token']
+    return HttpResponse(validated_mpesa_access_token)
+
+def lipa_na_mpesa_online(request):
     if request.method=="POST":
-        transaction_number=request.POST.get('phone_number')
-        cell= str(254)+str(int(transaction_number))
-        remiting_number=int(cell)
-        amount=request.POST.get('pay')
-        print("Post")
-        form= Payment_Form(request.POST)
-        if form.is_valid():
-            print("Valid")
-            amount= form.cleaned_data.get("amount")
-            Payment_reason= form.cleaned_data.get("Payment_reason")
-            phone_number= form.cleaned_data.get("phone_number")
-            access_token = MpesaAccessToken.validated_mpesa_access_token
-            api_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+            transaction_number=request.POST.get('phone_number')
+            cell= str(254)+str(int(transaction_number))
+            remiting_number=int(cell)
+            amount=request.POST.get('amount')
+            print("Post")
+            form= Payment_Form(request.POST)
+            if form.is_valid():
+                access_token = MpesaAccessToken.validated_mpesa_access_token
+            api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
             headers = {"Authorization": "Bearer %s" % access_token}
             request = {
-                "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
-                "Password": LipanaMpesaPpassword.decode_password,
-                "Timestamp": LipanaMpesaPpassword.lipa_time,
-                "TransactionType": "CustomerPayBillOnline",
-                "Amount": int(amount),
-                "PartyA": remiting_number, 
-                "PartyB": LipanaMpesaPpassword.Business_short_code,
-                "PhoneNumber": remiting_number, 
-                "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
-                "AccountReference": "Clinterpol Security and Forensic Consulting Limited",
-                "TransactionDesc": "Clinterpol"
-            }
-
+            "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+            "Password": LipanaMpesaPpassword.decode_password,
+            "Timestamp": LipanaMpesaPpassword.lipa_time,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": int(amount),
+            "PartyA": remiting_number,  # replace with your phone number to get stk push
+            "PartyB": LipanaMpesaPpassword.Business_short_code,
+            "PhoneNumber": remiting_number,  # replace with your phone number to get stk push
+            "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+            "AccountReference": "Clinterpol",
+            "TransactionDesc": "Clinterpol Security and Forensics Consulting Limited"
+        }
             response = requests.post(api_url, json=request, headers=headers)
             return HttpResponse(f'Kindly check your phone {remiting_number} and enter mpesa pin to succesfully pay to Clinterpol Security and Forensic Consulting Limited')
     else:
@@ -107,60 +128,6 @@ def successView(request):
     return HttpResponse('Success! Thank you for your message.')
     
 
-def getAccessToken(request):
-    consumer_key = 'CHrYGbGBVAqyzI35W84USB9ro8T9m22r'
-    consumer_secret = 'Zv53iyFKaJIS6AJ7'
-    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-
-    r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
-    mpesa_access_token = json.loads(r.text)
-    validated_mpesa_access_token = mpesa_access_token['access_token']
-
-    return HttpResponse(validated_mpesa_access_token)
-
-
-@csrf_exempt
-def register_urls(request):
-    access_token = MpesaAccessToken.validated_mpesa_access_token
-    api_url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
-    headers = {"Authorization": "Bearer %s" % access_token}
-    options = {"ShortCode": LipanaMpesaPpassword.Business_short_code,
-               "ResponseType": "Completed",
-               "ConfirmationURL": "http://127.0.0.1:8000/api/v1/c2b/confirmation",
-               "ValidationURL": "http://127.0.0.1:8000/api/v1/c2b/validation"}
-    response = requests.post(api_url, json=options, headers=headers)
-    return HttpResponse(response.text)
-@csrf_exempt
-def call_back(request):
-    pass
-@csrf_exempt
-def validation(request):
-    context = {
-        "ResultCode": 0,
-        "ResultDesc": "Accepted"
-    }
-    return JsonResponse(dict(context))
-@csrf_exempt
-def confirmation(request):
-    mpesa_body =request.body.decode('utf-8')
-    mpesa_payment = json.loads(mpesa_body)
-    payment = MpesaPayment(
-        first_name=mpesa_payment['FirstName'],
-        last_name=mpesa_payment['LastName'],
-        middle_name=mpesa_payment['MiddleName'],
-        description=mpesa_payment['TransID'],
-        phone_number=mpesa_payment['MSISDN'],
-        amount=mpesa_payment['TransAmount'],
-        reference=mpesa_payment['BillRefNumber'],
-        organization_balance=mpesa_payment['OrgAccountBalance'],
-        type=mpesa_payment['TransactionType'],
-    )
-    payment.save()
-    context = {
-        "ResultCode": 0,
-        "ResultDesc": "Accepted"
-    }
-    return JsonResponse(dict(context))
 
 
 # def whatsapp(request):
